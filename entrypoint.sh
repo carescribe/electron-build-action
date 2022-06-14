2edcfa9f3458ec7da01f52da83a4ee34c9ae5475
Tom Cartwright <tecartwright@gmail.com>
2022-06-14 20:50:06 +0100

    Define wine config location

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf /github/home
-mkdir /github/home
+mkdir "$HOME/wine"
+export WINEPREFIX="$HOME/wine"
 
 echo "The wine container has started: "
 pwd
 echo "Output of ls -lah"
 ls -lah
-echo "HOME DIR $HOME"
+echo "HOME DIR $HOME with WINEPREFIX $WINEPREFIX"
 DISABLE_ESLINT_PLUGIN=true yarn build:windows:production -p never
