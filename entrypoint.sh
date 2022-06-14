7aafe41ba58ffb93fbc07114e71773a5c2422388
Tom Cartwright <tecartwright@gmail.com>
2022-06-14 16:26:19 +0100

    Update debugging information

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "The wine container has started"
-echo "ls -lah"
+echo "The wine container has started: "
+pwd
+echo "Output of ls -lah"
+ls -lah
 DISABLE_ESLINT_PLUGIN=true yarn build:windows:production -p never
